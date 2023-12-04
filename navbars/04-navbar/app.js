269e0f35164024aaf3c1de8bed265de49e27a472
derrioncodes <derrioncodes@gmail.com>
2023-12-04 16:05:58 -0500

    added submenu on hover

--- navbars/04-navbar/app.js
+++ navbars/04-navbar/app.js
@@ -11,9 +11,51 @@ closeMobileBtn.addEventListener('click', () => {
     mobileContainer.classList.remove('active');
 })
 
+
+
+// START OF SUBMENU
+import sublinks from './data.js';
+const headerContainer = document.querySelector('header');
+const hero = document.querySelector('.hero');
+const submenu = document.querySelector('.submenu');
+const menuLinkBtns = [...document.querySelectorAll('.menu-links')];
+
+menuLinkBtns.forEach((btn) => {
+    btn.addEventListener('mouseover', (e) => {
+        const page = e.currentTarget.textContent;
+        const tempPage = sublinks.find((link) => link.page === page);
+        
+
+        if(tempPage.hasSublinks == true){
+            submenu.classList.add('show');  
+        } else if(tempPage.hasSublinks == false){
+            submenu.classList.remove('show');
+        }
+    })
+})
+
+
+
+
+
+headerContainer.addEventListener('mouseover', function (e) {
+    if (!e.target.classList.contains('menu-links')) {
+      submenu.classList.remove('show');
+    }
+});
+
+hero.addEventListener('mouseover', function (e) {
+    if (!e.target.classList.contains('menu-links')) {
+      submenu.classList.remove('show');
+    }
+});
+
 window.addEventListener("resize", function(event) {
-    if(document.body.clientWidth > 600 ){
+    if(document.body.clientWidth > 900 ){
         mobileContainer.classList.remove('active');
+    }
+    if(document.body.clientWidth < 900 ){
+        submenu.classList.remove('show');
     }
     // console.log(document.body.clientWidth + ' wide by ' + document.body.clientHeight+' high');
 })
---
+++ navbars/04-navbar/data.js
@@ -0,0 +1,139 @@
+const sublinks = [
+    {
+        page: 'Home',
+        hasSublinks: false,
+    },
+    {
+        page: 'Women',
+        hasSublinks: true,
+        links: [
+            { 
+                collectionTitle: 'Trending',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Products',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Gifting',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Collections',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            
+        ],
+    },
+
+    {
+        page: 'Men',
+        hasSublinks: true,
+        links: [
+            { 
+                collectionTitle: 'Trending',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Products',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Gifting',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Collections',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            
+        ],
+    },
+
+    {
+        page: 'Accessories',
+        hasSublinks: true,
+        links: [
+            { 
+                collectionTitle: 'Trending',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Products',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Gifting',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            { 
+                collectionTitle: 'Collections',
+                collectionSublinks: [
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'Gifts for her', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                    {label: 'New Realeses', url: '#'},
+                ]    
+            },
+            
+        ],
+    },
+];
+
+export default sublinks;
